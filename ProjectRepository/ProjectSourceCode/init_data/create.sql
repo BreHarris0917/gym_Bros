bd76a8597a384289c38990040ae939e20f5fe0df
Idaly Gomez-Pena <idgo2238@colorado.edu>
2024-11-06 09:24:03 -0700

    Changes made to create.sql

--- ProjectRepository/ProjectSourceCode/init_data/create.sql
+++ ProjectRepository/ProjectSourceCode/init_data/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users(
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     firstName vARCHAR(50),
